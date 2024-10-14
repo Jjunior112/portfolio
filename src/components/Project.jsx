49687966ba1d8b43f7c1b151a38d9a7f2e9ac173
jjunior112 <juarezgjcr@gmail.com>
2024-10-14 05:59:17 -0300

    Ajuste no card de projetos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,19 +1,25 @@
+import { FaGithub } from 'react-icons/fa'
+import { RiEarthFill } from "react-icons/ri";
 
-
-const Project = ({   name, description}) => {
+const Project = ({ name, image, description,linkGithub,linkSite }) => {
 
     return (
         <div className='project' >
 
             <h2>{name}</h2>
 
+            <img src={image} alt="" />
+
             <div className='description' >
 
                 <p>{description}</p>
 
             </div>
+            <div >
+                <a href={linkGithub} target="_blank" rel="noopener noreferrer"><FaGithub className='github' /></a>
+                <a href={linkSite} target="_blank" rel="noopener noreferrer"><RiEarthFill className='site' /></a>
+            </div>
 
-           
         </div>
 
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,22 @@
 import Project from "./Project"
 
 const Projects = () => {
+    const projectsList = [{
+        name: "Projetos Landing Pages",
+        image: "profile.png",
+        description: "API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos.",
+        linkGithub: "https://github.com/Jjunior112",
+        linkSite: "https://react-icons.github.io/react-icons/search/#q=earth"
+    },
+    {
+        name: "Projetos usando API",
+        image: "profile.png",
+        description: "API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos.",
+        linkGithub: "https://github.com/Jjunior112",
+        linkSite: "https://react-icons.github.io/react-icons/search/#q=earth"
+    }]
+
+
     return (
         <section className="projects">
 
@@ -9,24 +25,12 @@ const Projects = () => {
                 <a>Contact me</a>
             </div>
             <div className="listProjects">
-                <Project name='Projetos Landing Pages'
-
-                    description="API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos." />
-                <Project name='Projetos Landing Pages'
-
-                    description="API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos." />
-                <Project name='Projetos Landing Pages'
-
-                    description="API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos." />
-                <Project name='Projetos Landing Pages'
-
-                    description="API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos." />
-                <Project name='Projetos Landing Pages'
-
-                    description="API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos." />
-                <Project name='Projetos Landing Pages'
-
-                    description="API de Cardápio, uma aplicação Node.js que utiliza o MongoDB como banco de dados para gerenciar pratos e tipos de pratos em um cardápio. Esta API oferece endpoints simples para adicionar, remover e alterar informações sobre os pratos, bem como adicionar novos tipos de pratos." /></div>
+                {
+                    projectsList.map((project) => (
+                        <Project name={project.name} image={project.image} description={project.description} linkGithub={project.linkGithub} linkSite={project.linkSite} />
+                    ))
+                }
+            </div>
 
 
         </section>
