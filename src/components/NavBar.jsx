a3086686e1a98180d7e5a7d60d83137422992a7d
jjunior112 <juarezgjcr@gmail.com>
2024-10-16 17:53:57 -0300

    ajustes para deploy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,8 +10,6 @@ const NavBar = () => {
 
                 <li><a href="https://github.com/Jjunior112" target="_blank" title='Link para perfil do GitHub' rel="noopener noreferrer" ><FaGithub className='github' /></a></li>
 
-                <li><a href="https://jjunior112.github.io/portfolioDev/Curriculo.pdf" download='curriculo.pdf' title='Link para download do curriculo em pdf' rel="noopener noreferrer"><FaFileDownload className='download' /></a></li>
-
             </ul>
         </nav>
     )
